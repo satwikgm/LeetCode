d2efecac17e6a2fe14fb3b7451f04ce762f5f328
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-04-08 10:15:02 +0530

    Time: 5 ms (68.56%), Space: 8.6 MB (75.00%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node* , Node*> m;
+    Node* cloneGraph(Node* node) 
+    {
+        if(!node) 
+        {
+            return NULL;
+        }
+        Node* newNode = new Node(node->val , {});
+        m[node] = newNode;
+        queue<Node*> q;
+        q.push(node);
+        while(!q.empty())
+        {
+            Node* curr = q.front();
+            q.pop();
+            for(Node* adj : curr->neighbors)
+            {
+                if(m.find(adj) == m.end())
+                {
+                    m[adj] = new Node(adj->val , {});
+                    q.push(adj);
+                }
+                m[curr]->neighbors.push_back(m[adj]);
+            }
+        }
+        return newNode;
+    }
+};
