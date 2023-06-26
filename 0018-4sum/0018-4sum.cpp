6d580b067f79e86d8b6b4cea984a375344b40c9c
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-06-26 22:03:13 +0530

    Time: 85 ms (63.55%), Space: 13.1 MB (93.62%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) 
+    {
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])
+            {
+                continue;   
+            }
+            for(int j=i+1;j<n;j++)
+            {
+                if(j>i+1 && nums[j]==nums[j-1])
+                {
+                    continue;
+                }
+                long long x = (long long)target - (long long)nums[i] - (long long)nums[j];
+                int s = j+1 , e=n-1;
+                while(s<e)
+                {
+                    if(nums[s]+nums[e]==x)
+                    {
+                        ans.push_back({nums[i],nums[j],nums[s],nums[e]});
+                        while(s+1<n && nums[s]==nums[s+1])
+                        {
+                            s++;
+                        }
+                        while(e-1>=0 && nums[e]==nums[e-1])
+                        {
+                            e--;
+                        }
+                        s++;
+                        e--;
+                    }
+                    else if(nums[s]+nums[e]>x)
+                    {
+                        e--;
+                    }
+                    else
+                    {
+                        s++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
