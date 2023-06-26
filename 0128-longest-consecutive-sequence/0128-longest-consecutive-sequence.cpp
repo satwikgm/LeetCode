13cb14ed667bba1fdc310b8e6824191627ee79e9
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-06-26 18:44:28 +0530

    Time: 676 ms (22.57%), Space: 60.7 MB (61.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -3,20 +3,25 @@ public:
     int longestConsecutive(vector<int>& nums) 
     {
         if(nums.size()<=1)
-            return nums.size();
-        sort(nums.begin(),nums.end());
-        int ans=1,temp_ans=1;
-        for(int i=1;i<nums.size();i++)
         {
-            if(nums[i] == nums[i-1]+1)
+            return nums.size();
+        }
+        unordered_set<int> s(nums.begin(),nums.end());
+        int ans=0,curr=0;
+        int temp_ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(s.find(nums[i] - 1) == s.end())
             {
-                temp_ans++;
-            }
-            else if(nums[i] != nums[i-1])
-            {
+                curr = nums[i];
                 temp_ans=1;
             }
-            ans = max(ans , temp_ans);
+            while(s.find(curr+1) != s.end())
+            {
+                curr = curr + 1;
+                temp_ans++;
+            }
+            ans=max(ans,temp_ans);
         }
         return ans;
     }
