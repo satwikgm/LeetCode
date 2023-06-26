af17ac261dc887fa0acd613615f0019a91549c66
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-06-26 22:11:24 +0530

    Time: 74 ms (76.18%), Space: 13 MB (93.62%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,37 +2,36 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) 
     {
-        set<vector<int>> st;
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         int n=nums.size();
         for(int i=0;i<n;i++)
         {
-            // if(i>0 && nums[i]==nums[i-1])
-            // {
-            //     continue;   
-            // }
+            if(i>0 && nums[i]==nums[i-1])
+            {
+                continue;   
+            }
             for(int j=i+1;j<n;j++)
             {
-                // if(j>i+1 && nums[j]==nums[j-1])
-                // {
-                //     continue;
-                // }
+                if(j>i+1 && nums[j]==nums[j-1])
+                {
+                    continue;
+                }
                 long long x = (long long)target - (long long)nums[i] - (long long)nums[j];
                 int s = j+1 , e=n-1;
                 while(s<e)
                 {
                     if(nums[s]+nums[e]==x)
                     {
-                        st.insert({nums[i],nums[j],nums[s],nums[e]});
-                        // while(s+1<n && nums[s]==nums[s+1])
-                        // {
-                        //     s++;
-                        // }
-                        // while(e-1>=0 && nums[e]==nums[e-1])
-                        // {
-                        //     e--;
-                        // }
+                        ans.push_back({nums[i],nums[j],nums[s],nums[e]});
+                        while(s+1<n && nums[s]==nums[s+1])
+                        {
+                            s++;
+                        }
+                        while(e-1>=0 && nums[e]==nums[e-1])
+                        {
+                            e--;
+                        }
                         s++;
                         e--;
                     }
@@ -46,10 +45,6 @@ public:
                     }
                 }
             }
-        }
-        for(auto it : st)
-        {
-            ans.push_back(it);
         }
         return ans;
     }
