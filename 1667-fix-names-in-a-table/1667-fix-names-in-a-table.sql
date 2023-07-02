085b905f13040162247e679cf02d320090494602
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-07-02 18:46:05 +0530

    Time: 1416 ms (30.39%), Space: 0B (100.00%) - LeetHub

---
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+select user_id , concat(upper(substr(name,1,1)),lower(substr(name,2))) as name
+from Users
+order by user_id;
+
+
+# Users table:
+# +---------+-------+
+# | user_id | name  |
+# +---------+-------+
+# | 1       | aLice |
+# | 2       | bOB   |
+# +---------+-------+
+# Output: 
+# +---------+-------+
+# | user_id | name  |
+# +---------+-------+
+# | 1       | Alice |
+# | 2       | Bob   |
+# +---------+-------+
