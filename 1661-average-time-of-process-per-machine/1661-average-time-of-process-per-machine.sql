75a6e89b903ec0e526e8f4ee0606bc1a39457212
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-07-03 13:02:09 +0530

    Time: 493 ms (46.01%), Space: 0B (100.00%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,33 @@
+# # Write your MySQL query statement below
+
+select t1.machine_id , round(avg(t2.timestamp-t1.timestamp),3) as processing_time
+from Activity t1 join Activity t2
+on t1.machine_id=t2.machine_id and t1.process_id=t2.process_id and t1.activity_type='start' and t2.activity_type='end'
+group by t1.machine_id;
+
+
+# Activity table:
+# +------------+------------+---------------+-----------+
+# | machine_id | process_id | activity_type | timestamp |
+# +------------+------------+---------------+-----------+
+# | 0          | 0          | start         | 0.712     |
+# | 0          | 0          | end           | 1.520     |
+# | 0          | 1          | start         | 3.140     |
+# | 0          | 1          | end           | 4.120     |
+# | 1          | 0          | start         | 0.550     |
+# | 1          | 0          | end           | 1.550     |
+# | 1          | 1          | start         | 0.430     |
+# | 1          | 1          | end           | 1.420     |
+# | 2          | 0          | start         | 4.100     |
+# | 2          | 0          | end           | 4.512     |
+# | 2          | 1          | start         | 2.500     |
+# | 2          | 1          | end           | 5.000     |
+# +------------+------------+---------------+-----------+
+# Output: 
+# +------------+-----------------+
+# | machine_id | processing_time |
+# +------------+-----------------+
+# | 0          | 0.894           |
+# | 1          | 0.995           |
+# | 2          | 1.456           |
+# +------------+-----------------+
