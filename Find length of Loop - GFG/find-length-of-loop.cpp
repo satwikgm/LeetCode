8ea7987ecf977d3c4ae0ee4937ec1d06d1692d6d
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-07-05 14:04:32 +0530

    Added solution - LeetHub

---
+++ Find length of Loop - GFG/find-length-of-loop.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+int countNodesinLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        cout<< countNodesinLoop(head) << endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+//Function to find the length of a loop in the linked list.
+int countNodesinLoop(struct Node *head)
+{
+    Node* s=head , *f=head;
+    bool fl=0;
+        while(f && f->next)
+        {
+            s=s->next;
+            f=f->next->next;
+            if(s==f)
+            {
+                fl=1;
+                break;
+            }
+        }
+        if(!fl)
+        {
+            return 0;
+        }
+        s=head;
+        while(s!=f)
+        {
+            s=s->next;
+            f=f->next;
+        }
+        int ctr=1;
+        f=f->next;
+        while(s!=f)
+        {
+            f=f->next;
+            ctr++;
+        }
+        // cout<<ctr<<endl;
+        return ctr;
+}
