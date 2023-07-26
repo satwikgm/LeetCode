88ed06263f22f79e5fc408a63afc6a98187fa924
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-07-26 15:00:45 +0530

    Time: 19 ms (44.41%), Space: 14.4 MB (29.35%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     set<string> dict;
     unordered_map<int,int> dp;
-    bool fun(string s , int i) {
+    bool fun(string s , int i , vector<int> &dp) {
         if(i==s.size()) {
             return 1;
         }
-        if(dp.find(i) != dp.end()) {
+        if(dp[i] != -1) {
             return dp[i];
         }
         for(int k=i;k<s.size();k++) {
             string temp = s.substr(i,k-i+1);
-            if( (  dict.find(temp) != dict.end()) && fun(s,k+1)) {
+            if( (  dict.find(temp) != dict.end()) && fun(s,k+1,dp)) {
                 return dp[i] = 1;
             }
         }
@@ -20,9 +20,10 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         string sm="leetcode";
         cout<<sm.substr(1,4);
+        vector<int> dp(s.size()+1,-1);
         for(auto it : wordDict) {
             dict.insert(it);
         }
-        return fun(s,0);
+        return fun(s,0,dp);
     }
 };
