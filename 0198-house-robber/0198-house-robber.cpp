716b114631096d0f360e1cdaac1201fad373c895
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-07-29 22:32:47 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (74.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int fun(vector<int> &nums , int i)
+    int fun(vector<int> &nums , int n)
     {
-        int dp[nums.size()];
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<nums.size();i++) {
-            dp[i]=max(dp[i-2] + nums[i] , dp[i-1] );
+        int dp[n+1];
+        memset(dp,0,sizeof(dp));
+        dp[1]=nums[0];
+        dp[2]=max(nums[0],nums[1]);
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=max(dp[i-1] , nums[i-1]+dp[i-2]);
         }
-        return dp[nums.size()-1];
+        return dp[n];
     }
     int rob(vector<int>& nums) 
     {
@@ -16,6 +18,6 @@ public:
         if(n==1) {
             return nums[0];
         }
-        return fun(nums,0);
+        return fun(nums,n);
     }
 };
