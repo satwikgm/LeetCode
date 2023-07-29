1bd056694efbc393ea20d503db3c6ce2594c4642
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-07-29 18:06:53 +0530

    Time: 64 ms (99.24%), Space: 25.5 MB (93.79%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,85 +1,45 @@
 class Solution {
 public:
-    int totalOnes=0;
-    int boundaryOnes=0;
-    bool fun(vector<vector<int>>& grid , int i , int j , int &ones , bool vis[501][501])
-    {
-        bool fl=0;
-        
+    int numEnclaves(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        
         queue<pair<int,int>> q;
-        q.push({i,j});
-        
-        if(i==0 || j==0 || i==m-1 || j==n-1)
+        int ans=0;
+        for(int i=0;i<m;i++)
         {
-            fl=1;
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    ans++;
+                    if((i==0 || j==0 || i==m-1 || j==n-1))
+                    {
+                        q.push({i,j});
+                    }
+                }
+            }
         }
-        
         int dx[] = {-1,1,0,0};
-        int dy[] = {0,0,-1,1};
-        
+        int dy[] = {0,0,1,-1};
         while(q.size())
         {
             auto p = q.front();
             q.pop();
             int i = p.first;
             int j = p.second;
-            if(i==0 || j==0 || i==m-1 || j==n-1)
-            {
-                fl=1;
-            }
-            vis[i][j]=1;
+            if(grid[i][j]==0)
+                continue;
+            --ans;
+            grid[i][j]=0;
             for(int k=0;k<4;k++)
             {
-                int di = i + dx[k];
-                int dj = j + dy[k];
-                if(di>=0 && di<m && dj>=0 && dj<n && !vis[di][dj] && grid[di][dj]==1)
+                int nr = i+dx[k] , nc = j+dy[k];
+                if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1)
                 {
-                    q.push({di,dj});
-                    vis[di][dj]=1;
-                    ones++;
-                    if(di==0 || dj==0 || di==m-1 || dj==n-1)
-                    {
-                        fl=1;;
-                    }
+                    q.push({nr,nc});
                 }
             }
         }
-        return fl;
-    }
-    int numEnclaves(vector<vector<int>>& grid) 
-    {
-        int m=grid.size(),n=grid[0].size();
-        bool vis[501][501];
-        memset(vis,0,sizeof(vis));
-        
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j])
-                {
-                   totalOnes++; 
-                }
-            }
-        }
-        
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(!vis[i][j] && grid[i][j])
-                {
-                    int ones=1;
-                    if(fun(grid,i,j,ones,vis))
-                    {
-                        boundaryOnes += ones;
-                    }
-                }
-            }
-        }
-        return totalOnes - boundaryOnes;
+        return ans;
     }
 };
