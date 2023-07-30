fd98b7b44c53717cb7a81778a5a0b49ad932fee7
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-07-30 18:08:34 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -17,11 +17,40 @@ class Solution{
 	    if(s1[m-1]==s2[n-1]) return memo[m][n] = fun(s1,s2,m-1,n-1);
 	    return memo[m][n] =  1+min(fun(s1,s2,m,n-1),fun(s1,s2,m-1,n));
 	}
+	int funn(string s1, string s2 , int m , int n)
+	{
+	    int dp[m+1][n+1];
+	    for(int i=0;i<=m;i++)
+	    {
+	        for(int j=0;j<=n;j++)
+	        {
+	            if(i==0)
+	            {
+	                dp[i][j]=j;
+	            }
+	            if(j==0)
+	            {
+	                dp[i][j]=i;
+	            }
+	        }
+	    }
+	    for(int i=1;i<=m;i++)
+	    {
+	        for(int j=1;j<=n;j++)
+	        {
+	            if(s1[i-1]==s2[j-1])
+	                dp[i][j]=dp[i-1][j-1];
+	            else
+	                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+1;
+	        }
+	    }
+	    return dp[m][n];
+	}
 	int minOperations(string s1, string s2) 
 	{ 
 	    // Your code goes here
 	    memset(memo,-1,sizeof(memo));
-	    return fun( s1, s2 , s1.size() , s2.size());
+	    return funn( s1, s2 , s1.size() , s2.size());
 	} 
 };
 
