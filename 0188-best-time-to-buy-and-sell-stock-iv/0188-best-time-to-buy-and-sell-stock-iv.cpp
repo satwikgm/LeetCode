c11fb530dc0799d7858ce99e25fd341992e587d6
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-07-30 18:41:23 +0530

    Time: 26 ms (48.15%), Space: 14.5 MB (16.70%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -19,6 +19,32 @@ public:
                         ,  fun(a,0,k,i+1,n,dp) );
         }
     }
+    int funn(vector<int> &a , int k)
+    {
+        int n=a.size();
+        int dp[n+1][2][k+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=n;i++)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                for(int lim=0;lim<=k;lim++)
+                {
+                    if(buy)
+                    {
+                        dp[i][buy][lim] = max(-a[i-1] + dp[i-1][0][lim]  , 
+                                            dp[i-1][1][lim]    );
+                    }
+                    else
+                    {
+                        dp[i][buy][lim] = max(a[i-1] + dp[i-1][1][lim-1]  , 
+                                            dp[i-1][0][lim]    );
+                    }
+                }
+            }
+        }
+        return dp[n][1][k];
+    }
     int maxProfit(int k, vector<int>& prices) 
     {
         vector<vector<vector<int>>> dp(prices.size()+1 , vector<vector<int>>(2,vector<int>(k+1,-1)));
