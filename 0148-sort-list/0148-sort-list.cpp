51f085c3ca1b14b45d75d0a8dd308e5b69896d4d
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-07-31 10:03:26 +0530

    Time: 165 ms (98.18%), Space: 73.9 MB (43.27%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,52 +10,43 @@
  */
 class Solution {
 public:
-    ListNode* mergeList(ListNode* l1 , ListNode* l2)
+    ListNode* merge(ListNode* l1 , ListNode* l2)
     {
-        ListNode* ptr = new ListNode(-1);
-        ListNode* curr=ptr;
+        ListNode*  head = new ListNode(-1);
+        ListNode* curr = head;
         while(l1 && l2)
         {
-            if(l1->val > l2->val)
+            if(l1->val <= l2->val)
             {
-                curr->next = l2;
-                l2=l2->next;
+                curr->next = l1;
+                curr = curr->next;
+                l1=l1->next;
             }
             else
             {
-                curr->next = l1;
-                l1=l1->next;
+                curr->next = l2;
+                curr = curr->next;
+                l2=l2->next;
             }
-            curr=curr->next;
         }
-        if(l1)
-        {
-            curr->next = l1;
-            // l1=l1->next;
-        }
-        if(l2)
-        {
-            curr->next=l2;
-            // l2=l2->next;
-        }
-        return ptr->next;
+        if(l1) curr->next = l1;
+        if(l2) curr->next = l2;
+        return head->next;
     }
     ListNode* sortList(ListNode* head) 
     {
-        if(!head || !head->next)
-        {
-            return head;
-        }
-        ListNode *s=head,*f=head,*prev;
+        if(!head || !head->next) return head;
+        ListNode* s=head , *f=head;
+        ListNode* temp;
         while(f && f->next)
         {
-            prev=s;
+            temp=s;
             s=s->next;
             f=f->next->next;
         }
-        prev->next=NULL;
+        temp->next = NULL;
         ListNode* l1 = sortList(head);
         ListNode* l2 = sortList(s);
-        return mergeList(l1,l2);
+        return merge(l1,l2);
     }
 };
