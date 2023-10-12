fc10bd2b161f188e4079588784779bd4ba98b900
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-10-12 11:45:23 +0530

    Time: 157 ms (69.89%), Space: 67.1 MB (67.97%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div912-sort-an-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div912-sort-an-array.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div912-sort-an-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div912-sort-an-array.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
-    void merge(vector<int> &arr , int lo , int mid , int hi)
-    {
-        int n1 = mid-lo+1;
-        int n2 = hi-mid;
-        int arr1[n1];
-        int arr2[n2];
-        for(int i=0;i<n1;i++)
-        {
-            arr1[i] = arr[lo+i];
+    void merge(vector<int> &arr , int lo , int mid , int hi) {
+        int n1 = mid-lo+1 , n2 = hi-mid;
+        int a1[n1] , a2[n2];
+        for(int i=0;i<n1;i++) {
+            a1[i] = arr[lo+i];
         }
-        for(int j=0;j<n2;j++)
-        {
-            arr2[j] = arr[mid+1+j];
+        for(int i=0;i<n2;i++) {
+            a2[i] = arr[mid+1+i];
         }
-        int i=0,j=0,k=lo;
+        int i=0 , j=0 , k=lo;
         while(i<n1 && j<n2)
         {
-            if(arr1[i]>=arr2[j])
-            {
-                arr[k++]=arr2[j++];
-            }
+            if(a1[i] <= a2[j])
+                arr[k++] = a1[i++];
             else
-            {
-                arr[k++]=arr1[i++];
-            }
+                arr[k++] = a2[j++];
         }
-        while(i<n1) arr[k++] = arr1[i++];
-        while(j<n2) arr[k++] = arr2[j++];
+        while(i<n1) arr[k++] = a1[i++];
+        while(j<n2) arr[k++] = a2[j++];
     }
-    void mergeSort(vector<int> &arr , int lo , int hi)
-    {
-        if(lo>=hi)
+    void mergeSort(vector<int> &arr , int lo , int hi) {
+        if(lo>=hi) {
             return;
+        }
         int mid = (lo+hi)/2;
         mergeSort(arr,lo,mid);
         mergeSort(arr,mid+1,hi);
         merge(arr,lo,mid,hi);
-        
     }
     vector<int> sortArray(vector<int>& nums) {
         mergeSort(nums,0,nums.size()-1);
