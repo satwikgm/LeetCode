e0cc2c45aa871ac6ca674081ed15c5580e95fecf
Satwik <97330754+satwikgm@users.noreply.github.com>
2023-10-27 19:12:57 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (37.57%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    void fun(int ind , vector<int> &nums , set<vector<int>> &ans , vector<int> v)
+    set<vector<int>> st;
+    void fun(vector<int> &curr , vector<int> &arr , int i , int n)
     {
-        if(ind==nums.size())
+        if(i==n)
         {
-            ans.insert(v);
+            st.insert(curr);
             return;
         }
-        fun(ind+1,nums,ans,v);
-        v.push_back(nums[ind]);
-        fun(ind+1,nums,ans,v);
-        v.pop_back();
+        // pick
+        curr.push_back(arr[i]);
+        fun(curr,arr,i+1,n);
+        curr.pop_back();
+        // not pick
+        fun(curr,arr,i+1,n);
     }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) 
-    {
-        sort(nums.begin(),nums.end());
-        set<vector<int>> ans;
-        vector<int> v;
-        fun(0,nums,ans,v);
-        vector<vector<int>> answer;
-        for(auto it : ans)
+    vector<vector<int>> subsetsWithDup(vector<int>& arr) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        sort(arr.begin(),arr.end());
+        fun(curr,arr,0,arr.size());
+        for(auto it : st)
         {
-            answer.push_back(it);
+            ans.push_back(it);
         }
-        return answer;
+        return ans;
     }
 };
